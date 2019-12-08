039fd06cd9f9fe9d3451c512bea3624034f3cdc0
宫新程 <gongxincheng@ruigushop.com>
2019-12-08 22:58:26 +0800

    practice non blocking nio 2

--- nio-demo-1/src/com/gxc/nio/demo1/TestNonBlockingNio.java
+++ nio-demo-1/src/com/gxc/nio/demo1/TestNonBlockingNio.java
@@ -2,17 +2,14 @@ package com.gxc.nio.demo1;
 
 import org.junit.Test;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.time.LocalDateTime;
 import java.util.Iterator;
-import java.util.Scanner;
 
 /**
  * 非阻塞式NIO网络通讯
@@ -25,7 +22,8 @@ public class TestNonBlockingNio {
     @Test
     public void client() throws Exception {
         // 1：获取Socket通道
-        SocketChannel sChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 10000));
+        SocketChannel sChannel = SocketChannel.open(
+                new InetSocketAddress("127.0.0.1", 10000));
 
         // 2：切换成非阻塞模式
         sChannel.configureBlocking(false);
@@ -42,53 +40,50 @@ public class TestNonBlockingNio {
         sChannel.close();
     }
 
+    /**
+     * 服务端
+     */
     @Test
     public void server() throws Exception {
         // 1：获取通道
-        ServerSocketChannel ssChannel = ServerSocketChannel.open();
-        // 2：切换为非阻塞模式
-        ssChannel.configureBlocking(false);
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        // 2：切换到非阻塞模式
+        serverSocketChannel.configureBlocking(false);
         // 3：绑定连接
-        ssChannel.bind(new InetSocketAddress(10000));
+        serverSocketChannel.bind(new InetSocketAddress(10000));
         // 4：获取选择器
         Selector selector = Selector.open();
-        // 5：将通道注册到选择器上，并且指定"监听接收事件", (SelectionKey.OP_ACCEPT)监听通道的连接状态
-        ssChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        // 6：轮询式的获取选择器 已经准备就绪的事件
+        // 5：将通道注册到选择器上，并且指定"监听接收事件"
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        // 6：轮询式的获取选择器上已经"准备就绪"的事件
         while (selector.select() > 0) {
-            // 7：获取当前选择器中所有注册的"选择键（已就绪的监听事件）"
+            // 7：获取当前selector中所有注册的"选择键（已就绪的监听事件）"
             Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
             while (iterator.hasNext()) {
                 // 8：获取准备"就绪"的事件
-                SelectionKey selectedKey = iterator.next();
-                // 9：判断具体是什么事件准备就绪
-                if (selectedKey.isAcceptable()) {
-                    // 10：若"接收就绪"，获取客户端连接
-                    SocketChannel socketChannel = ssChannel.accept();
-                    // 11：切换非阻塞模式
-                    socketChannel.configureBlocking(false);
-                    // 12：将该通道注册到选择器上
-                    socketChannel.register(selector, SelectionKey.OP_READ);
-                } else if (selectedKey.isReadable()) {
-                    // 13：获取当前选择器上"读就绪"状态的通道
-                    SocketChannel socketChannel = (SocketChannel) selectedKey.channel();
-
+                SelectionKey selectionKey = iterator.next();
+                // 9：判断具体是哪种事件类型
+                if(selectionKey.isAcceptable()) {
+                    // 10：若接收事件"就绪"，获取客户端连接
+                    SocketChannel sChannel = serverSocketChannel.accept();
+                    // 11：将客户端通道切换成非阻塞模式
+                    sChannel.configureBlocking(false);
+                    // 12：将该 客户端通道 注册到Selector上，监听"读就绪"状态
+                    sChannel.register(selector, SelectionKey.OP_READ);
+                } else if (selectionKey.isReadable()) {
+                    // 13：获取当前Selector上"读就绪"的通道
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
                     // 14：读取数据
                     ByteBuffer buffer = ByteBuffer.allocate(1024);
                     while (socketChannel.read(buffer) != -1) {
                         buffer.flip();
-                        System.out.println(
-                                new String(buffer.array(), 0, buffer.limit()));
+                        System.out.println(new String(buffer.array(), 0, buffer.limit()));
                         buffer.clear();
                     }
                 }
-                // 15：取消选择键(SelectionKey)
+                // 15：取消选择键 SelectionKey
                 iterator.remove();
             }
         }
-
     }
-
-
 }
