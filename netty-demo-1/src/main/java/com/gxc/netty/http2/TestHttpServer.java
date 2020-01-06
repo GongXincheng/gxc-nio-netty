8106da298a41cfb982136c96acd4511e47cb1b66
宫新程 <gongxincheng@ruigushop.com>
2020-01-06 22:06:01 +0800

    http demo-2

---
+++ netty-demo-1/src/main/java/com/gxc/netty/http2/TestHttpServer.java
@@ -0,0 +1,57 @@
+package com.gxc.netty.http2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author GongXincheng
+ * @date 2020-01-06 19:49
+ */
+public class TestHttpServer {
+
+    public static void main(String[] args) {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+
+                            // HttpServerCodec 说明：HttpServerCodec 是Netty提供处理http的编解码器
+                            pipeline.addLast("MyHttpServerCodec", new HttpServerCodec());
+                            pipeline.addLast("MyTestHttpServerHandler", new TestHttpServerHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
--- netty-demo-1/src/main/java/com/gxc/netty/http2/TestHttpServerHandler.java
+++ netty-demo-1/src/main/java/com/gxc/netty/http2/TestHttpServerHandler.java
@@ -1,39 +1,28 @@
-package com.gxc.netty.http;
+package com.gxc.netty.http2;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * TODO：Http 处理器
- * 1：SimpleChannelInboundHandler 是 ChannelInboundHandlerAdapter
- * 2：HttpObject 客户端和服务器端相互通讯的数据被封装成 HttpObject 类型
- *
  * @author GongXincheng
- * @date 2020-01-05 21:13
+ * @date 2020-01-06 20:07
  */
 @Slf4j
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
-    /**
-     * 读取客户端的数据.
-     *
-     * @param ctx ChannelHandlerContext
-     * @param msg HttpObject
-     */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        // 判断 msg 是不是 HttpRequest 请求
         if (msg instanceof HttpRequest) {
             HttpRequest httpRequest = (HttpRequest) msg;
 
@@ -42,22 +31,17 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
             log.info("请求的uri：[{}]", httpRequest.uri());
             log.info("请求方法：[{}]\n", httpRequest.method());
 
+            ByteBuf byteBuf = Unpooled.copiedBuffer("Hello Client！", CharsetUtil.UTF_8);
 
+            HttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
 
-            // 获取信息给浏览器，[HTTP协议]
-            ByteBuf content = Unpooled.copiedBuffer("Hello, 我是Netty服务器", CharsetUtil.UTF_8);
-
-            // 构造一个http相应，即 HttpResponse
-            FullHttpResponse httpResponse = new DefaultFullHttpResponse(
-                    HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
-
-            // 设置相应头
             httpResponse.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json;charset=utf-8")
-                    .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+                    .set(HttpHeaderNames.CONTENT_LENGTH, byteBuf.readableBytes());
 
-            // 将构建好的 response 返回
+            // 相应消息给浏览器
             // TODO：此时ctx是 [DefaultChannelHandlerContext] 的对象
             ctx.writeAndFlush(httpResponse);
         }
     }
+
 }
